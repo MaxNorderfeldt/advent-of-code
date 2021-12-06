804937c687ad549d6a0f1ec93cacd3d99881a5a7
max.norderfeldt <max.norderfeldt@gmail.com>
2021-12-06 15:45:56 +0100

    Prepare for d6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,24 +11,39 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Main {
     public static void main(String[] args) {
-
+        Long[] lanternFishes = new Long[9];
+        //create empty copy of lanternfish, copy over one variable at a time and shift it one step to the left
+        //0 goes to 6 and creates a new 8
+        //Start from the last position and move left if(i != 1) do thing
+        for (int i = 0; i < lanternFishes.length; i++) {
+            lanternFishes[i] = 0L;
+        }
+        int days = 18;
         try {
-            File myObj = new File("/Users/max/Documents/AOCInput/inputDay5.txt");
+            File myObj = new File("C:\\Temp\\inputDay6.txt");
             Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                myReader.nextLine();
-            }
-            myObj = new File("/Users/max/Documents/AOCInput/inputDay5.txt");
-            myReader = new Scanner(myObj);
-            myReader.useDelimiter(",| -> |\n");
+            myReader.useDelimiter("[,]");
 
             while (myReader.hasNext()) {
-
+                int fish = myReader.nextInt();
+                lanternFishes[fish]++;
             }
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
+        for (int i = 0; i < days; i++) {
+            long tempFish=lanternFishes[8];
+            for (int j = 7; j > 1; j--) {
+                lanternFishes[j]=tempFish;
+                tempFish=lanternFishes[j-1];
+            }
+        }
+
+        System.out.println(lanternFishes.length);
+        for (int i = 0; i < lanternFishes.length; i++) {
+            System.out.println(lanternFishes[i]);
+        }
     }
 }
