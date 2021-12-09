a4b7a2d547ff6b6a387815443b1e20880c537551
max <max.norderfeldt@gmail.com>
2021-12-09 19:21:10 +0100

    day8 finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,64 +11,215 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> input = new ArrayList<String>();
-        ArrayList<String> output = new ArrayList<String>();
+        ArrayList<String> data = new ArrayList<String>();
+        ArrayList<String> outputs = new ArrayList<String>();
+        ArrayList<String> inputs = new ArrayList<String>();
 
         try {
-            File myObj = new File("C:\\Temp\\inputDay8.txt");
+            File myObj = new File("/Users/max/Documents/AOCInput/inputDay8.txt");
             Scanner myReader = new Scanner(myObj);
-            myReader.useDelimiter("\\|");
-
-            int counter=0;
+            int counter = 0;
 
 
             System.out.println();
 
             while (myReader.hasNext()) {
-                if(counter%2==0) {
-                    input.add(myReader.next());
-                }else {
-                    output.add(myReader.next());
-                }
-                counter++;
+                data.add(myReader.nextLine());
             }
-
-
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
-        for (String test: input
-             ) {
-            System.out.println(test+" input");
-
+        String number = "";
+        long sum = 0;
+        for (String line : data
+        ) {
+            String[] temp = line.split("\\|");
+            inputs.add(temp[0]);
+            outputs.add(temp[1]);
         }
-        for (String test: output
-             ) {
-            System.out.println(test +" output");
-        }
+        int countNumbers = 0;
+        for (int i = 0; i < inputs.size(); i++) {
+            String[] temp = inputs.get(i).split(" ");
+            String one = "", seven = "", four = "", eight = "";
+            ArrayList<String> sixes = new ArrayList<String>();
+            ArrayList<String> fives = new ArrayList<String>();
+            for (String input : temp
+            ) {
+                int l = input.length();
+                //code below gets the known numbers
 
-        System.out.println(output.size()+"test inputsize");
-        int countKnownOutput=0;
-        int outputSize=0;
-        ArrayList<String> inputArray= new ArrayList<String>();
-
-        for (int i = 0; i < output.size(); i++) {
-            String[] tempOutput = output.get(i).split(" ");
-            for (String outputString : tempOutput
-                 ) {
-                System.out.println(outputString+" test");
-                outputSize=outputString.length();
-                System.out.println(outputSize);
-                if(outputSize==2 || outputSize==3 || outputSize==4 || outputSize == 7) {
-                    countKnownOutput++;
-                    System.out.println("hit");
+                if (l == 2) {
+                    one = input;
+                } else if (l == 3) {
+                    seven = input;
+                } else if (l == 4) {
+                    four = input;
+                } else if (l == 7) {
+                    eight = input;
+                } else if (l == 6) {
+                    sixes.add(input);
+                } else if (l == 5) {
+                    fives.add(input);
                 }
 
             }
+            char a = ' ';
+            String tempChar = seven;
+            for (char part : one.toCharArray()
+            ) {
+                tempChar = tempChar.replace(String.valueOf(part), "");
+            }
+            a = tempChar.charAt(0);
+
+
+            char g = ' ';
+            //code below gets the nine so that i can use it to get G
+            String nine = "";
+            for (String sixLetterNumber : sixes
+            ) {
+                int counter = 0;
+                for (int j = 0; j < four.length(); j++) {
+                    String testI = String.valueOf(four.charAt(j));
+                    if (sixLetterNumber.contains(testI)) {
+                        counter++;
+                    } else {
+                        break;
+                    }
+                    if (counter == 4) {
+                        nine = sixLetterNumber;
+                    }
+                }
+            }
+
+            tempChar = nine;
+            for (char part : four.toCharArray()
+            ) {
+                tempChar = tempChar.replace(String.valueOf(part), "");
+                tempChar = tempChar.replace(String.valueOf(a), "");
+            }
+            g = tempChar.charAt(0);
+
+            char e = ' ';
+
+            tempChar = eight;
+            for (char part : nine.toCharArray()
+            ) {
+                tempChar = tempChar.replace(String.valueOf(part), "");
+            }
+            e = tempChar.charAt(0);
+
+
+            String three = "";
+            for (String fiveLetterNumber : fives
+            ) {
+                int counter = 0;
+                for (int j = 0; j < one.length(); j++) {
+                    String testI = String.valueOf(one.charAt(j));
+                    if (fiveLetterNumber.contains(testI)) {
+                        counter++;
+                    } else {
+                        break;
+                    }
+                    if (counter == 2) {
+                        three = fiveLetterNumber;
+                    }
+                }
+            }
+            tempChar = three;
+            char d = ' ';
+            for (char part : seven.toCharArray()
+            ) {
+                tempChar = tempChar.replace(String.valueOf(part), "");
+                tempChar = tempChar.replace(String.valueOf(g), "");
+            }
+            d = tempChar.charAt(0);
+
+            tempChar = four;
+            char b = ' ';
+            for (char part : one.toCharArray()
+            ) {
+                tempChar = tempChar.replace(String.valueOf(part), "");
+                tempChar = tempChar.replace(String.valueOf(d), "");
+            }
+            b = tempChar.charAt(0);
+            String de = String.valueOf(d) + e;
+            String six = "";
+            for (String sixLetterNumber : sixes
+            ) {
+                int counter = 0;
+                for (int j = 0; j < de.length(); j++) {
+                    String testI = String.valueOf(de.charAt(j));
+                    if (sixLetterNumber.contains(testI)) {
+                        counter++;
+                    } else {
+                        break;
+                    }
+                    if (counter == 2) {
+                        six = sixLetterNumber;
+                    }
+                }
+            }
+            tempChar = six;
+            char f = ' ';
+            tempChar = tempChar.replace(String.valueOf(g), "");
+            tempChar = tempChar.replace(String.valueOf(a), "");
+            tempChar = tempChar.replace(String.valueOf(d), "");
+            tempChar = tempChar.replace(String.valueOf(e), "");
+            tempChar = tempChar.replace(String.valueOf(b), "");
+
+            f = tempChar.charAt(0);
+
+            tempChar = seven;
+            char c = ' ';
+            tempChar = tempChar.replace(String.valueOf(a), "");
+            tempChar = tempChar.replace(String.valueOf(f), "");
+
+            c = tempChar.charAt(0);
+
+            System.out.println(outputs.size());
+
+            for (String t : outputs.get(i).split(" ")
+            ) {
+                //System.out.println(t);
+            }
+            number = "";
+            String[] splitOutput = outputs.get(i).split(" ");
+
+            for (int j = 0; j < splitOutput.length; j++) {
+                String word = splitOutput[j];
+                // System.out.println(word);
+                int l = word.length();
+                if (l == 2) {
+                    number += 1;
+                } else if (l == 3) {
+                    number += 7;
+                } else if (l == 4) {
+                    number += 4;
+                } else if (l == 7) {
+                    number += 8;
+                } else if (word.contains(Character.toString(a)) && word.contains(Character.toString(b)) && word.contains(Character.toString(c)) && word.contains(Character.toString(d)) && word.contains(Character.toString(f)) && word.contains(Character.toString(g))) {
+                    number += 9;
+                } else if (word.contains(Character.toString(a)) && word.contains(Character.toString(b)) && word.contains(Character.toString(c)) && word.contains(Character.toString(e)) && word.contains(Character.toString(f)) && word.contains(Character.toString(g))) {
+                    number += 0;
+                } else if (word.contains(Character.toString(a)) && word.contains(Character.toString(b)) && word.contains(Character.toString(d)) && word.contains(Character.toString(e)) && word.contains(Character.toString(f)) && word.contains(Character.toString(g))) {
+                    number += 6;
+                } else if (word.contains(Character.toString(a)) && word.contains(Character.toString(b)) && word.contains(Character.toString(d)) && word.contains(Character.toString(f)) && word.contains(Character.toString(g))) {
+                    number += 5;
+                } else if (word.contains(Character.toString(a)) && word.contains(Character.toString(c)) && word.contains(Character.toString(d)) && word.contains(Character.toString(f)) && word.contains(Character.toString(g))) {
+                    number += 3;
+                }else if (word.contains(Character.toString(a)) && word.contains(Character.toString(c)) && word.contains(Character.toString(d)) && word.contains(Character.toString(e)) && word.contains(Character.toString(g))) {
+                    number += 2;
+                }
+
+                System.out.println(number);
+            }
+            System.out.println(sum += Integer.valueOf(number));
         }
-        System.out.println(countKnownOutput);
+
 
     }
+
+
 }
+
