518aecb270f5d3d3036f56796c1fc498cf4ce5f9
MaxNorderfeldt <32924829+MaxNorderfeldt@users.noreply.github.com>
2021-12-19 23:10:02 +0100

    Day14 part 2 WIP

--- src/mn/adventofcode/year2021/Day14.java
+++ src/mn/adventofcode/year2021/Day14.java
@@ -2,7 +2,6 @@ package mn.adventofcode.year2021;
 
 import mn.adventofcode.year2021.utilities.HelperMethods;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -10,59 +9,77 @@ import java.util.Set;
 
 public class Day14 {
     public static long getResult() {
-        HashMap<String, String> pairInsertions = new HashMap<String, String>();
+        HashMap<String, Character> pairInsertions = new HashMap<String, Character>();
 
         ArrayList<String> input = HelperMethods.readInput(14);
-        ArrayList<Character> polymerTemplate = new ArrayList<>();
-
-        for (char c : input.get(0).toCharArray()) {
-            chars.add(c);
+        ArrayList<Character>[] polymerTemplate = new ArrayList[20];
+        //ArrayList<Character> polymerTemplate = new ArrayList<>();
+        String firstRow = input.get(0);
+        for (int i = 0; i < firstRow.length(); i++) {
+            polymerTemplate[i] = new ArrayList<Character>();
+            polymerTemplate[i].add(firstRow.charAt(i));
         }
+
         for (int i = 2; i < input.size(); i++) {
-            String[] splitRow = input.get(i).split(" -> ");
-            pairInsertions.put(splitRow[0], splitRow[1]);
+            String row = input.get(i);
+            String[] splitRow = row.split(" -> ");
+            pairInsertions.put(splitRow[0], row.charAt(6));
         }
 
         int steps = 40;
+        //NBBBCNCCNBBNBNBBCHBHHBCHB
         for (int step = 0; step < steps; step++) {
-            System.out.println("Step" + step);
-            ArrayList<String> commandsToRun=new ArrayList<>();
-            for (long i = 0; i < polymerTemplate.length()-1; i++) {
-                String command = polymerTemplate.substring((int)i, (int)i+2);
-                commandsToRun.add(command);
+            System.out.println("step " + step);
+            ArrayList<Character> tempPolymer = new ArrayList<>();
+            for (int i = 0; i < polymerTemplate.length; i++) {
+                tempPolymer = new ArrayList<>(polymerTemplate[i]);
+                int addCounter=1;
+                for (int j = 0; j < polymerTemplate[i].size(); j++) {
+
+                    if (j < polymerTemplate[i].size() - 1) {
+                        tempPolymer.add(addCounter,pairInsertions.get("" + polymerTemplate[i].get(j) + polymerTemplate[i].get(j + 1)));
+                        addCounter+=2;
+                    } else{
+                    }
+                }
+                polymerTemplate[i] = tempPolymer;
             }
-            polymerTemplate=;
-            String command="";
-            for (int i = 0; i < commandsToRun.size(); i++) {
-                command = commandsToRun.get(i);
-                String polymerToAdd =command.charAt(0) + pairInsertions.get(command) ;
-                polymerTemplate+=polymerToAdd;
+            for (int i = 0; i < polymerTemplate.length - 1 ; i++) {
+                polymerTemplate[i].add(pairInsertions.get("" + polymerTemplate[i].get(polymerTemplate[i].size() - 1) + polymerTemplate[i + 1].get(0)));
             }
-            polymerTemplate+=command.charAt(1);
         }
 
-        char characters[] = polymerTemplate.toCharArray();
+
+
         Set<Character> uniques = new HashSet<Character>();
-        for (char c: characters) {
-            uniques.add(c);
+        for (int i = 0; i < polymerTemplate.length; i++) {
+            for (char c : polymerTemplate[i]) {
+                uniques.add(c);
+            }
         }
+
         long maxLetter = 0;
         long minLetter = Long.MAX_VALUE;
         for (char letter : uniques
-             ) {
-            long counter=0;
-            for (long i = 0; i < polymerTemplate.length(); i++) {
-                if(polymerTemplate.charAt((int)i)==letter) {
-                    counter++;
+        ) {
+            long counter = 0;
+            for (int i = 0; i < polymerTemplate.length; i++) {
+                for (int j = 0; j < polymerTemplate[i].size(); j++) {
+                    if (polymerTemplate[i].get(j) == letter) {
+                        counter++;
+                    }
                 }
             }
-            if(counter>maxLetter) {
-                maxLetter=counter;
+            if (counter > maxLetter) {
+                maxLetter = counter;
             }
-            if(counter<minLetter) {
-                minLetter=counter;
+            if (counter < minLetter) {
+                minLetter = counter;
             }
         }
-        return maxLetter-minLetter;
+
+        System.out.println("Old: NC-NB-CH-B-0");
+        System.out.println(maxLetter-minLetter);
+        return 0;
     }
 }
